952f1294fcfc6e4c48e26acac228a462d3efbc67
xXJadenXx <jadenl1@learn.cssd.ab.ca>
2018-09-06 14:16:09 -0600

    Updated main storyboard with title and buttons with field text

---
+++ Github Haden Part 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Github Haden Part 2
+//
+//  Created by Jaden Luzon on 2018-09-06.
+//  Copyright © 2018 Jaden Luzon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
